3760305b0b0f17234564ec918e2e33e907707cfd
Brooks Garrett <brooks@brooksgarrett.com>
2016-11-25 15:56:54 -0500

    Lecture 11 complete.

--- public/app.jsx
+++ public/app.jsx
@@ -1,22 +1,29 @@
 var Greeter = React.createClass({
     getDefaultProps: function () {
         return {
-            name: 'Guest'
+            name: 'Guest',
+            message: 'This is from the component.'
         };
     },
     render: function () {
         var name = this.props.name;
+        var message = this.props.message;
 
         return (
             <div>
                 <h1>Hello {name}!</h1>
-                <p>This is from the component.</p>
+                <p>{message}</p>
+
+
             </div>
         );
     }
 });
 
+var firstName = 'Brooks';
+var message = 'This is my message';
+
 ReactDOM.render(
-    <Greeter />,
+    <Greeter name={firstName} message={message} />,
     document.getElementById('app')
 );
