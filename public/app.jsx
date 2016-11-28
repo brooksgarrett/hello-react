cc2b92cbbcfa4b4d29bd8bd3e81773737225b840
Brooks Garrett <brooks@brooksgarrett.com>
2016-11-27 22:39:55 -0500

    Lecture 14 complete.

--- public/app.jsx
+++ public/app.jsx
@@ -1,3 +1,27 @@
+var GreeterMessage = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1>Some H1</h1>
+                <p>Some p</p>
+            </div>
+        );
+    }
+});
+
+var GreeterForm = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <form>
+                    <input type="text" ref="name" />
+                    <button>Some button</button>
+                </form>
+            </div>
+        );
+    }
+});
+
 var Greeter = React.createClass({
     getDefaultProps: function () {
         return {
@@ -28,10 +52,9 @@ var Greeter = React.createClass({
                 <h1>Hello {name}!</h1>
                 <p>{message}</p>
 
-                <form onSubmit={this.onButtonClick}>
-                    <input type="text" ref="name" />
-                    <button>Set Name</button>
-                </form>
+                <GreeterMessage />
+                <GreeterForm />
+                
             </div>
         );
     }
